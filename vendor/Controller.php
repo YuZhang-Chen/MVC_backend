3b28aebfd59cc77e7379e4d1015dd470ea4dd3af
CHEN,YU-ZHANG <c112156205@nkust.edu.tw>
2025-06-09 11:17:46 +0800

    Remove vendor directory from Git and ignore it

--- vendor/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Vendor;
-
-abstract class Controller {
-    protected static function response($status, $message, $result=null) {
-        return ['status' => $status, 'message' => $message, 'result' => $result];
-    }
-}
